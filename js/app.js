e1102d6af5c33048735b68451a338a5d48399976
vlad moisei <vladmoisei@yahoo.com>
2018-05-03 13:23:28 +0100

    Add scalability to project

--- js/app.js
+++ js/app.js
@@ -5,24 +5,49 @@
 // 	4: "img/cat4.jpg",
 // 	5: "img/cat5.jpg"
 // };
+function Cat(name = "", img = "", clickCounter = 0) {
+	this.name = name;
+	this.img = img;
+	this.clickCounter = clickCounter;
+}
 
-var catList = ['cat1', 'cat2', 'cat3', 'cat4', 'cat5'];
+let pisica = new Cat("Gigi", "imag 1");
+var catList = [new Cat('Tom', 'img/cat1.jpg'), new Cat('Baghera','img/cat2.jpg'), new Cat('Bila','img/cat3.jpg'), new Cat('Ben','img/cat4.jpg'), new Cat('Blacky','img/cat5.jpg')];
 
-let asideMenu = document.querySelector('.nav');
+let asideMenu = document.querySelector('.nav .nav__list');
 let main = document.querySelector('main');
 let catName = document.querySelector('.catName');
 let catImage = document.querySelector('.catImage');
 let catClicks = document.querySelector('.catClicks');
 
-var counterElemNav = 0;
-for(let imag of catList) {
-	counterElemNav++;
-	 var contentImag = document.createElement('div');
-	 contentImag.textContent = `Cat ${counterElemNav}`;
-	 contentImag.addEventListener('click', function() {
-	 	catName.textContent = this.textContent;
-	 	catImage.src = "";
-	 });
+for(let i = 0; i < catList.length; i++) {
+	 var cat = catList[i];
+	 var contentImag = document.createElement('li');
+	 contentImag.textContent = `Cat: ${catList[i].name}`;
+
+	  // ... artificiu pentru a retine obiectul pe care s-a dat click
+	  // si pentru a putea sa ii adaugam event listener
+    contentImag.addEventListener('click', (function(catCopy) {
+        return function() {
+            //alert(numCopy.name);
+            catName.textContent = catCopy.name;
+            catImage.src = catCopy.img;
+            catClicks.textContent = catCopy.clickCounter;
+        };
+    })(cat));
+
 	 asideMenu.appendChild(contentImag);
 
 };
+
+catImage.addEventListener('click', function() {
+	catList.forEach(cat => {
+		//alert(`${catName.textContent} - ${cat.name}`);
+		if (catName.textContent === cat.name) {
+			//alert(catName);
+			cat.clickCounter++;
+			catClicks.textContent = cat.clickCounter;
+			//break;
+		};
+	});
+});
